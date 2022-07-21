c1e9100e63a6242594315214e80bf5ec42ec219c
Kyle K. Lin <gazer.kanlin@gmail.com>
2022-07-21 08:29:10 +0800

    Update

--- src/main/java/uia/dao/ObjectMapper.java
+++ src/main/java/uia/dao/ObjectMapper.java
@@ -2,6 +2,8 @@ package uia.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface ObjectMapper<T> {
 
@@ -12,6 +14,18 @@ public interface ObjectMapper<T> {
         @Override
         public Object read(ResultSet rs) throws SQLException {
             return null;
+        }
+    }
+
+    public static class StrList implements ObjectMapper<List<String>> {
+
+        @Override
+        public List<String> read(ResultSet rs) throws SQLException {
+            ArrayList<String> values = new ArrayList<>();
+            while (rs.next()) {
+                values.add(rs.getString(1));
+            }
+            return values;
         }
 
     }
